be088896066ab6e5b5ad9ac259c9d08bafed99f0
= <=>
2024-05-01 07:59:47 -0500

    Adding home about footer sections

--- src/routes/about/About.jsx
+++ src/routes/about/About.jsx
@@ -1,11 +1,65 @@
 import React from 'react'
 
 import './about.css'
+import aboutMe from '../../assets/aboutMe.jpg'
+import tec from '../../assets/tec.jpg'
+import software from '../../assets/software.jpg'
+import games from '../../assets/games.jpg'
 
 export const About = () => {
     return (
         <>
-            <h1>About</h1>
+            <div className='about-container'>
+                <div className='about-info'>
+                    <div className='about-image'>
+                        <img className='aboutMe' src={ aboutMe } alt="about-me" />
+                    </div>
+                    <div className='info'>
+                        <h3 className='title-about'>Acerca de mi</h3>
+                        <h5 className='description-about'>Soy desarrollador de software, tanto 
+                            back-end como front-end, apasionado por 
+                            la tecnologia y el conocimiento, aquí te 
+                            comparto algunas de las tecnologías con las
+                            que trabajo y algunos de mis trabajos. 
+                        </h5>
+                        <p></p>
+                    </div>
+                </div>
+                <div className='tecs'>
+                    <h2 className='title-tec'>Front-end</h2>
+                    <img className='image-tec' src={ tec } alt="image-tec" />
+                    <p className='description-tec'>
+                        Para el front-end, suelo utilizar una tecnología bastante
+                        útil, que es REACT JS, nos permite desarrollar sitios web
+                        reactivos, las famosas SPA, donde la experiencia de usuario
+                        es bastante agradable sin necesidad de recargar la página.
+                    </p>
+                </div>
+
+                <div className='tecs-back'>
+                    <h2 className='title-tec'>Back-end</h2>
+                    <img className='image-tec' src={ software } alt="image-tec" />
+                    <p className='description-tec'>
+                        Para el back-end trabajo con dos tecnologías especificas,
+                        que son: Express js y nest js, son dos tecnologías que utilizan
+                        Node.js para correr aplicaciones del lado del back, se usan
+                        para crear software totalmente escalable y que pueda tener una 
+                        respuesta bastante rapida.
+                    </p>
+                </div>
+
+                <div className='tecs'>
+                    <h2 className='title-tec'>Video games</h2>
+                    <img className='image-tec' src={ games } alt="image-tec" />
+                    <p className='description-tec'>
+                        Siempre me ha gustado y llamado la atención el 
+                        desarrollo de videojuego, lo he hecho de manera
+                        totalmente autodidacta utilizando una tecnología
+                        llamada PYGAME, que usa el lenguaje de programación 
+                        python.
+                    </p>
+                </div>
+            </div>
         </>
     )
 }
--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 import './home.css'
 import businnes from '../../assets/businnes.jpg'
+import tecnology from '../../assets/tecnology.jpg'
+import programming from '../../assets/programming.jpg'
+
+import Slider from './Corousel'
 
 export const Home = () => {
     return (
@@ -23,6 +27,44 @@ export const Home = () => {
                         </p>
                     </div>
                 </article>
+
+                <article className='article'>
+                    <div className='image-article'>
+                        <img className='businnes' src={ tecnology } alt="article-image" />
+                    </div>
+                    <div className='description-article'>
+                        <h2 className='title-article'>Tecnologías con las que trabaja</h2>
+                        <p className='subtitle'>
+                            DevCat está constantemente actualizando su stack de 
+                            tecnologias, para ofrecerte el mejor servicio porsible,
+                            Siendo totalmente autodidacta y tener la capacidad de 
+                            adaptar las nuevas mejoras y tecnologias más avanzadas
+                            a sus proyectos.
+                        </p>
+                    </div>
+                </article>
+
+                <article className='article'>
+                    <div className='image-article'>
+                        <img className='businnes' src={ programming } alt="article-image" />
+                    </div>
+                    <div className='description-article'>
+                        <h2 className='title-article'>Que hace DevCat?</h2>
+                        <p className='subtitle'>
+                            DevCat está altamente preparado para abordar cualquier tipo
+                            de proyecto, ya sea un sitio web totalmente básico, hasta un
+                            software mas avanzado, aplicaciones moviles, chatbots para tu
+                            negocio, ecommerce, etc.
+                        </p>
+                    </div>
+                </article>
+
+                <div className='works'>
+                    <h1 className='works-title'>Algunos de los trabajos de DevCat</h1>
+                    <div className='work-wrap'>
+                        <Slider />
+                    </div>
+                </div>
             </section>
         </>
     )
