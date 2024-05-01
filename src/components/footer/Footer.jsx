c504061479461cc98086e1a8518204770c230bd3
= <=>
2024-05-01 09:55:15 -0500

    Adding footer and contact section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import { FaFacebook } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
+
 
 import './footer.css'
 import logo from '../../assets/logo.png'
@@ -9,9 +14,20 @@ export const Footer = () => {
                 <div className='footer-container'>
                     <div className='footer-logo'>
                         <img className='logo-footer' src={ logo } alt="logo" />
+                        <h6>&copy; DevCat Todos los derechos reservados 2024</h6>
+                    </div>
+                    <div className='sections-footer'>
+                        <h6>Home</h6>
+                        <h6>About</h6>
+                        <h6>Services</h6>
+                        <h6>Contact</h6>
                     </div>
                     <div className='social-networks'>
-
+                        <h5>Síguenos en nuestras redes</h5>
+                        <a href='https://www.facebook.com/esteban.rojasm.3' target='_blank'><FaFacebook className='item-footer' /></a>
+                        <a href='https://github.com/spread23' target='_blank'><FaGithub className='item-footer' /></a>
+                        <a href='https://www.instagram.com/devcat23/' target='_blank'><FaInstagram className='item-footer' /></a>
+                        <a href='https://www.linkedin.com/in/julian-esteban-rojas-marin-849052a1/' target='_blank'><FaLinkedin className='item-footer' /></a>
                     </div>
                 </div>
             </footer>
--- src/routes/contact/Contact.jsx
+++ src/routes/contact/Contact.jsx
@@ -3,9 +3,30 @@ import React from 'react'
 import './contact.css'
 
 export const Contact = () => {
+    const handleOnSubmit = (e) => {
+        e.preventDefault()
+    }
     return (
         <>
-            <h1>Contact</h1>
+            <div className='contact-container'>
+                <div className='form-contact'>
+                    <form onSubmit={handleOnSubmit} className='form'>
+                        <div className='form-group'>
+                            <label htmlFor="name">Nombre</label>
+                            <input type="text" name='name' placeholder='Nombre...' />
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor="lastname">Apellido</label>
+                            <input type="text" name='lastname' placeholder='Apellido...' />
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor="description">Motivo de contacto</label>
+                            <textarea type="text" name='description' placeholder='Motivo...' />
+                        </div>
+                        <button className='btn-contact'>Enviar</button>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
